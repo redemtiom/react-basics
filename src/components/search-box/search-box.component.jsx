03e6627cdc39b68569a143078036e6c9ad441cc9
redemtiom <smar.pre.sky@gmail.com>
2022-09-06 15:09:25 -0500

    Added List of card, added custom cards, added google font, added styles

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,21 @@
+import { Component } from 'react'
+
+import './search-box.styles.css'
+
+class SearchBox extends Component {
+    render() {
+        //const {onChangeHandler} = this.props
+        return (
+            <div>
+                <input
+                    className={this.props.className}
+                    type="search"
+                    placeholder={this.props.placeholder}
+                    onChange={this.props.onChangeHandler}
+                />
+            </div>
+        )
+    }
+}
+
+export default SearchBox
